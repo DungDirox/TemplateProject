9ae241335dfba1900ed98d07df26e345f9f14dc4
DungDo <dung.do@dirox.net>
2021-08-19 20:11:45 +0700

    !Update: 3rd libs

--- TemplateProject/Common/GoogleHelper.swift
+++ TemplateProject/Common/GoogleHelper.swift
@@ -8,55 +8,44 @@
 
 import GoogleSignIn
 
-class GoogleHelper: NSObject, GIDSignInDelegate {
-    
-    static let clientID = "601387200679-mbe30uea2pir2d8f8htk30hhkuqvbk4v.apps.googleusercontent.com"
+class GoogleHelper {
     
     static let shared = GoogleHelper()
     
-    private let loginManager = GIDSignIn.sharedInstance()!
+    private let loginManager = GIDSignIn.sharedInstance
     
-    private var complete: ((Dictionary<String, Any>?, Error?)->())?
+    private let signInConfig = GIDConfiguration.init(clientID: "601387200679-mbe30uea2pir2d8f8htk30hhkuqvbk4v.apps.googleusercontent.com")
     
-    var isLogin: Bool {
-        if let user = loginManager.currentUser, let _ = user.authentication {
-            return true
-        }
-        return false
-    }
-    
-    private override init() {}
+    private init() {}
     
     func login(on vc: UIViewController, complete: @escaping (Dictionary<String, Any>?, Error?)->()) {
-        self.complete = complete
+        self.loginManager.restorePreviousSignIn { user, error in
+            if let user = user {
+                complete(self.fetchMe(user: user), nil)
+            } else {
+                self.loginManager.signIn(with: self.signInConfig, presenting: vc) { user, error in
+                    if let user = user {
+                        complete(self.fetchMe(user: user), nil)
+                    } else {
+                        complete(nil, error)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func fetchMe(user: GIDGoogleUser) -> Dictionary<String, Any> {
+        var data: Dictionary<String, Any> = [:]
+        data["userId"] = user.userID
+        data["name"] = user.profile?.name
+        data["email"] = user.profile?.email
+        data["accessToken"] = user.authentication.accessToken
         
-        self.loginManager.delegate = self
-        self.loginManager.presentingViewController = vc
-        self.loginManager.signIn()
+        return data
     }
     
     func logout() {
         self.loginManager.signOut()
-    }
-    
-    // MARK: - GIDSignInDelegate
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if !self.isLogin {
-            self.complete!(nil, nil)
-            return
-        }
-        
-        if let error = error {
-            self.complete!(nil, error)
-        } else {
-            var data: Dictionary<String, Any> = [:]
-            data["userId"] = user.userID
-            data["name"] = user.profile.name
-            data["email"] = user.profile.email
-            data["accessToken"] = user.authentication.accessToken
-            
-            self.complete!(data, nil)
-        }
     }
     
 }
